df832992977d1fd55330688bbd73d6a38b197266
Prabal Laad <praballaad17@gmail.com>
2023-03-17 21:31:15 +0530

    mongo update

--- startups/mongodb.js
+++ startups/mongodb.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
-const mongoURI = process.env.MONGOURI;
-// const mongoURI = "mongodb://localhost:27017/Touch";
+// const mongoURI = process.env.MONGOURI;
+const mongoURI = "mongodb://localhost:27017/Touch";
 
 function mongofunction(app) {
-
   (async function () {
     try {
       await mongoose.connect(mongoURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-        useFindAndModify: false
+        useFindAndModify: false,
       });
-      console.log('Connected to database');
+      console.log("Connected to database");
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   })();
 }
-
 
 module.exports = { mongoURI, mongofunction };
