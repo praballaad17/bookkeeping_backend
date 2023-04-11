9d7f512b5521e0362179c84fb5895eb8a7403178
Prabal Laad <praballaad17@gmail.com>
2023-04-11 10:43:26 +0530

    invoice changes

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,103 +1,139 @@
 const Invoice = require("../models/invoice");
 const Party = require("../models/party");
-const pdf = require('html-pdf');
+const pdf = require("html-pdf");
 const { validatePhoneNo } = require("../utils/validation");
 
-const pdfTemplate = require('../Tamplete/template2');
+const pdfTemplate = require("../Tamplete/template2");
 
 module.exports.addInvoice = async (req, res, next) => {
-    // const { invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total } = req.body;
-    const { itemlist, userId, invoiceNumber, type, total, partyId, date } = req.body
+  // const { invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total } = req.body;
+  const { itemlist, userId, invoiceNumber, type, total, partyId, date } =
+    req.body;
 
-    // const phoneNoError = validatePhoneNo(phoneNo);
-    // if (phoneNoError) return res.status(400).send({ error: phoneNoError });
+  // const phoneNoError = validatePhoneNo(phoneNo);
+  // if (phoneNoError) return res.status(400).send({ error: phoneNoError });
 
-    try {
-        // newInvoice = new Invoice({invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total});
-        newInvoice = new Invoice({ itemIds: itemlist, total, date, user: userId, type, party: partyId, invoiceNumber })
-        await newInvoice.save();
+  try {
+    // newInvoice = new Invoice({invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total});
+    newInvoice = new Invoice({
+      itemIds: itemlist,
+      total,
+      date,
+      user: userId,
+      type,
+      party: partyId,
+      invoiceNumber,
+    });
+    await newInvoice.save();
 
-        res.status(201).send(newInvoice);
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(201).send(newInvoice);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.getInvoiceDetails = async (req, res, next) => {
-    try {
-        const newInvoice = await Invoice.findOne({ _id: req.params.invoiceId }).populate("party")
-        res.send(newInvoice);
-    } catch (err) {
-        next(err);
-    }
-}
+  try {
+    const newInvoice = await Invoice.findOne({
+      _id: req.params.invoiceId,
+    }).populate("party");
+    res.send(newInvoice);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.createInvoicePdf = async (req, res) => {
-    try {
-        pdf.create(pdfTemplate(req.body), {}).toFile(`${__dirname}/result.pdf`, (err) => {
-            if (err) {
-                console.log(err);
-                res.send(Promise.reject());
-            }
-            res.send(Promise.resolve());
-        });
-    } catch (err) {
-        res.send(err);
-    }
-}
+  try {
+    pdf
+      .create(pdfTemplate(req.body), {})
+      .toFile(`${__dirname}/result.pdf`, (err) => {
+        if (err) {
+          console.log(err);
+          res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+      });
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 module.exports.getPdfInvoice = async (req, res) => {
-    try {
-        res.sendFile(`${__dirname}/result.pdf`)
-    } catch (err) {
-        res.send(err);
-    }
-}
+  try {
+    res.sendFile(`${__dirname}/result.pdf`);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 module.exports.getInvoiceUserId = async (req, res, next) => {
-    const { userId, type } = req.params
-    try {
-        const invoices = await Invoice.find({ type, user: userId }).populate("party", ["name", "balance"])
-        return res.status(200).send(invoices)
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-}
+  const { userId, type } = req.params;
+  try {
+    const invoices = await Invoice.find({ type, user: userId }).populate(
+      "party",
+      ["name", "balance"]
+    );
+    return res.status(200).send(invoices);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
 
 module.exports.updateInvoiceDetails = async (req, res, next) => {
-    const { invoiceId } = req.params
-    // const { invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total } = req.body;
-    const { itemlist, invoiceNumber, type, total, partyId, date } = req.body
-    // const phoneNoError = validatePhoneNo(phoneNo);
-    // if (phoneNoError) return res.status(400).send({ error: phoneNoError });
+  const { invoiceId } = req.params;
+  // const { invoiceID, shipedTo, shippingAddress, phoneNo, todayDate, dueDate, itemIds, subTotal, gstTax, Discount, total } = req.body;
+  const { itemlist, invoiceNumber, type, total, partyId, date } = req.body;
+  // const phoneNoError = validatePhoneNo(phoneNo);
+  // if (phoneNoError) return res.status(400).send({ error: phoneNoError });
 
-    try {
-        const updatedInvoice = await Invoice.findOneAndUpdate({ _id: invoiceId },
-            {
-                $set: {
-                    invoiceNumber: invoiceNumber,
-                    type: type,
-                    date: date,
-                    party: partyId,
-                    itemIds: itemlist,
-                    total: total,
-                }
-            }, {
-            new: true
-        });
-        res.status(201).send({
-            updatedInvoice
-        });
-    } catch (err) {
-        next(err);
-    }
-}
+  try {
+    const updatedInvoice = await Invoice.findOneAndUpdate(
+      { _id: invoiceId },
+      {
+        $set: {
+          invoiceNumber: invoiceNumber,
+          type: type,
+          date: date,
+          party: partyId,
+          itemIds: itemlist,
+          total: total,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(201).send({
+      updatedInvoice,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.deleteInvoice = async (req, res) => {
-    try {
-        const invoice = await Invoice.deleteOne({ _id: req.params.id })
-        return res.status(200).send("deleted")
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-}
+  try {
+    const invoice = await Invoice.deleteOne({ _id: req.params.id });
+    return res.status(200).send("deleted");
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+module.exports.searchInvoice = async (req, res) => {
+  console.log(req.params.searchKey);
+  try {
+    const invoice = await Invoice.find({
+      $or: [
+        // { party: req.params.searchKey },
+        { phoneNo: req.params.searchKey },
+        { invoiceNumber: { $regex: req.params.searchKey } },
+        { phoneNo: req.params.searchKey },
+      ],
+    });
+    return res.status(200).send(invoice);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-    addInvoice,
-    getInvoiceDetails,
-    updateInvoiceDetails,
-    getInvoiceUserId,
-    deleteInvoice,
-    createInvoicePdf,
-    getPdfInvoice,
-} = require("../controllers/invoiceController")
+  addInvoice,
+  getInvoiceDetails,
+  updateInvoiceDetails,
+  getInvoiceUserId,
+  deleteInvoice,
+  createInvoicePdf,
+  getPdfInvoice,
+  searchInvoice,
+} = require("../controllers/invoiceController");
 
-router.post('/addInvoice', addInvoice);
-router.get('/invoiceId/:invoiceId', getInvoiceDetails);
-router.post('/invoice-pdf', createInvoicePdf)
-router.get('/get-pdf-invoice', getPdfInvoice)
-router.get('/invoiceId/:userId/type/:type', getInvoiceUserId);
-router.put('/userId/:userId/invoiceId/:invoiceId', updateInvoiceDetails);
-router.delete('/invoice/:id', deleteInvoice);
+router.post("/addInvoice", addInvoice);
+router.get("/invoiceId/:invoiceId", getInvoiceDetails);
+router.post("/invoice-pdf", createInvoicePdf);
+router.get("/get-pdf-invoice", getPdfInvoice);
+router.get("/invoiceId/:userId/type/:type", getInvoiceUserId);
+router.put("/userId/:userId/invoiceId/:invoiceId", updateInvoiceDetails);
+router.delete("/invoice/:id", deleteInvoice);
+router.get("/search-invoice/:searchKey", searchInvoice);
 
 module.exports = router;
